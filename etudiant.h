9fb4abecc33178fe3cc44ad214ca4533cdd97bf4
gbale esther ruth <gbaleestherruth@gmail.com>
2026-03-03 00:52:32 +0100

    correction structure header et ajout prototypes

--- etudiant.h
+++ etudiant.h
@@ -2,6 +2,8 @@
 #ifndef ETUDIANT_H
 #define ETUDIANT_H
 
+ /*defintionde la structure*/
+
 typedef struct 
 {
     char nom[50];
@@ -15,7 +17,32 @@ typedef struct
 typedef struct  
 {
     etudiant *liste;
+
     int nmbretudiant; 
-}gestionetudiant;
+    int capacite;
+
+}basedonnees;
+
+                 /*prototype des fonctionprincipales*/
+
+
+/*initilisation et memoire*/              
+void initialiserdebase(basedonnees *bd);
+void liberermemoire(basedonnees *bd);
+
+/*gesttion de base de donne*/
+void ajoueretudiant(basedonnees *bd);
+void supprimeretudiant(basedonnees *bd, int id);
+void modifieretudiant(basedonnees *bd, int id);
+
+/*afficher et rechercher*/
+void afficheretudiant(const basedonnees *bd);
+void rechercheretudaint(const basedonnees *bd);
+
+/*tri et option fichier*/
+void trieretudaint(basedonnees *bd);
+void sauvegardebinaire(const basedonnees *bd, const char *nomfichier);
+void chargementbinaire(basedonnees *bd, const char *nomfichier);
+void expotercsv(const basedonnees *bd, const char *nomfichier);
 
 #endif
